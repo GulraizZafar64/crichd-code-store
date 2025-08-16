2d35755491448e1a99c9b103a5afd46960bdcb17
GulraizZafar67 <gulraizzafar@preesoft.com>
2025-08-16 07:24:13 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose")
 
-const uri="mongodb+srv://abdulrafay9797:Eueu_6464@netflix.2a03rl4.mongodb.net/?retryWrites=true&w=majority"
-// const uri="mongodb+srv://gulraizzafar77:OshnKGJUm8E9YmLV@cluster0.pr5kytk.mongodb.net/codestore?retryWrites=true&w=majority&appName=Cluster0"
+// const uri="mongodb+srv://abdulrafay9797:Eueu_6464@netflix.2a03rl4.mongodb.net/?retryWrites=true&w=majority"
+const uri="mongodb+srv://gulraizzafar77:OshnKGJUm8E9YmLV@cluster0.pr5kytk.mongodb.net/codestore?retryWrites=true&w=majority&appName=Cluster0"
 const connectDatabase=( )=>{
     mongoose.set("strictQuery", false);
     mongoose.connect(uri,{useUnifiedTopology:true,
